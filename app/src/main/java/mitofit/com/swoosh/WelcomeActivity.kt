561b020ede8b8f03fb0d63089bda7259f9cbd2a1
Amin nasseri <aminnaaseri@gmail.com>
2019-10-12 12:35:31 +0330

    Intent

--- app/src/main/java/mitofit/com/swoosh/WelcomeActivity.kt
+++ app/src/main/java/mitofit/com/swoosh/WelcomeActivity.kt
@@ -1,13 +1,21 @@
 package mitofit.com.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_getStarted.setOnClickListener{
+            val intentleague = Intent(this,LeagueActivity::class.java)
+            startActivity(intentleague)
+        }
+
 
 
     }
