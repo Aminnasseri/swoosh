61af149a1cd04d068058b121608a152afc19b1ea
Amin nasseri <aminnaaseri@gmail.com>
2019-10-12 15:19:24 +0330

    skillFinish

--- app/src/main/java/mitofit/com/swoosh/LeagueActivity.kt
+++ app/src/main/java/mitofit/com/swoosh/LeagueActivity.kt
@@ -1,12 +1,18 @@
 package mitofit.com.swoosh
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View){
+        val intentSkill = Intent(this,SkillActivity::class.java)
+        startActivity(intentSkill)
     }
 }
